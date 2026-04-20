7f43329a391256890f22af304f328c8912db8d7a
tbalu777 <tarczybalu@hotmail.com>
2026-04-20 11:59:21 +0200

    angular projekt

--- 00-Angular-intro/travel-blog/src/app/app.ts
+++ 00-Angular-intro/travel-blog/src/app/app.ts
@@ -1,5 +1,11 @@
 import { Component, signal } from '@angular/core';
 
+interface User{
+  name: string;
+  age: number;
+  isActive: boolean;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
@@ -9,5 +15,13 @@ import { Component, signal } from '@angular/core';
 export class App {
   protected readonly title = signal('travel-blog');
 
+  // users: User[] | undefined;
+
   numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+  users: User[] = [
+    { name: 'Alice', age: 30, isActive: true },
+    { name: 'Bob', age: 25, isActive: false },
+    { name: 'Charlie', age: 35, isActive: true }
+  ];
 }
